9d861e8c1113bb4e808b95992172922a0e509c07
Arish <arishchowdhury.2021@gmail.com>
2024-03-23 15:23:09 +0600

    i started creating a snake game

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import random
+
 import pygame
 import time
 
 pygame.init()
 
+font_style = pygame.font.SysFont(None, 50)
+
+
+def message(msg, color):
+    mesg = (font_style.render(msg, True, color))
+    dis.blit(mesg, [(dis_width / 2) - 85, (dis_height / 2) - 45])
+
+
 white = (255, 255, 255)
 black = (0, 0, 0)
+blue = (0, 0, 255)
 red = (255, 0, 0)
 dis_width = 800
 dis_height = 600
@@ -12,58 +23,79 @@ dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.update()
 pygame.display.set_caption("Snake Game")
 
-game_over = False
 
-x1 = dis_width/2
-y1 = dis_height/2
+def game_loop():
+    game_over = False
+    game_close = False
 
-x1_change = 0
-y1_change = 0
+    x1 = dis_width / 2
+    y1 = dis_height / 2
 
-clock = pygame.time.Clock()
+    x1_change = 0
+    y1_change = 0
 
-speed = 5
+    snake_speed = 10
+    snake_block = 10
 
-font_style = pygame.font.SysFont(None, 50)
+    food_x = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    food_y = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
 
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width/2, dis_height/2])
+    clock = pygame.time.Clock()
 
-while not game_over:
-    for event in pygame.event.get():
-        # print(event)
-        if event.type == pygame.QUIT:
+    while not game_over:
+        while game_close:
+            message("Game Over", red)
+
+            pygame.display.update()
+            # time.sleep(2)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        game_loop()
+
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    x1_change = 0
+                    y1_change = -snake_block
+                elif event.key == pygame.K_DOWN:
+                    x1_change = 0
+                    y1_change = snake_block
+
+        if x1 > dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_over = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x1_change = -speed
-                y1_change = 0
-            elif event.key == pygame.K_RIGHT:
-                x1_change = speed
-                y1_change = 0
-            elif event.key == pygame.K_UP:
-                x1_change = 0
-                y1_change = -speed
-            elif event.key == pygame.K_DOWN:
-                x1_change = 0
-                y1_change = speed
 
-    if x1 > dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-        game_over = True
-#we need to remove something
-    x1 += x1_change
-    y1 += y1_change
-    dis.fill(white)
-    pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(white)
+        # this rectangle is for the food
+        pygame.draw.rect(dis, blue, [food_x, food_y, snake_block, snake_block])
+        # this rectangle is for the snake
+        pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
+        pygame.display.update()
 
-    pygame.display.update()
+        if x1 == food_x and y1 == food_y:
+            print("yummy")
 
-    clock.tick(30)
+        clock.tick(snake_speed)
 
-message("Game Over", red)
-pygame.display.update()
-time.sleep(10)
+    time.sleep(5)
 
-pygame.quit()
-quit()
+    pygame.quit()
+    quit()
+
+
+if __name__ == '__main__':
+    game_loop()
